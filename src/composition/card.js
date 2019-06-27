7b4cd92d388997014a57ebef1425326b278ed38c
Tara Patel <t.jeanne.patel@gmail.com>
2019-06-27 10:35:53 -0500

    added delete card

--- src/composition/card.js
+++ src/composition/card.js
@@ -6,7 +6,7 @@ function Card(props) {
     const { title, content, id } = props
     return (
       <div className="Card" id={id}>
-        <button type="button">delete</button>
+        <button onClick={() => {props.handleDelete(props.id)}} type="button">delete</button>
         <h3>{title}</h3>
         <p>{content}</p>
       </div>
--- src/composition/list.js
+++ src/composition/list.js
@@ -6,7 +6,12 @@ import './list.css';
 function List(props) {
   const arrayOfCards = props.cardIds.map((letter) => {
     let cardObj = props.allCards[letter];
-    return (<Card key={cardObj.id} title={cardObj.title} content={cardObj.content} id={cardObj.id} />)
+    return (<Card 
+      handleDelete={props.handleDelete} 
+      key={cardObj.id}
+      title={cardObj.title} 
+      content={cardObj.content} 
+      id={cardObj.id} />)
   })
 
     return (
